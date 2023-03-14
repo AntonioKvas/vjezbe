9192961c978b6ad2dcac91094f68a8ee95e032b7
Antonio Kvas (akvas) <akvas@tvz.hr>
2023-03-14 20:09:54 +0100

    vjezba1

---
+++ vjezba1_2.py
@@ -0,0 +1,14 @@
+#Ova linija mora biti komentar!
+print('Dobrodošli', '!')
+# Napisati sljedeće izraze u jednoj liniji koda
+ime = input('Unesite vaše ime:')
+prezime = input('Unesite vaše prezime:')
+JMBAG = input('Unesite vaš JMBAG')
+# TODO zatražite od korisnika broj godina
+godine = input('Unesite vaše godine:')
+# TODO ime i prezime ispišite u navodnim znakovima
+print("Korisnik : ", ime, " ", prezime,end='!')
+
+
+
+
